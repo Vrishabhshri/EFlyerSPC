91b6280134d448e36931e3069897c8b64f980220
Vrishabhshri <vrishabhshri@gmail.com>
2025-03-10 14:17:24 -0400

    Gutting initialization

--- layout.tsx
+++ layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Hidden Gems",
-  description: "SPC Show Flyer",
+  title: "Vir Sind",
+  description: "Official artist webpage",
 };
 
 export default function RootLayout({
