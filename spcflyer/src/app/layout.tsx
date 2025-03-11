2f387aca2b7f3c436df6561e71a436b91aaf1dce
Vrishabhshri <vrishabhshri@gmail.com>
2025-03-10 21:45:07 -0400

    Initial commit

--- spcflyer/src/app/layout.tsx
+++ spcflyer/src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Hidden Gems",
-  description: "SPC Show Flyer",
+  title: "SPCFlyer",
+  description: "Hidden Gems",
 };
 
 export default function RootLayout({
@@ -13,7 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`antialiased`}
+      >
         {children}
       </body>
     </html>
--- spcflyer/src/app/page.tsx
+++ spcflyer/src/app/page.tsx
@@ -1,7 +1,13 @@
-"use client"
+import Image from "next/image";
 
 export default function Home() {
 
-  
+  return (
 
+    <div className="">
+      
+    </div>
+
+  );
+  
 }
